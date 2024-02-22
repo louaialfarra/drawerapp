9d6be3b6e07840334acb00035c483ec5d1c49b87
louaialfarra <156582409+louaialfarra@users.noreply.github.com>
2024-02-23 01:53:37 +0300

    Initial commit

---
+++ constants/Draweritems.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    name: "home",
+    iconType: "Feather",
+    iconName: "house",
+  },
+  {
+    name: "sun",
+    iconType: "Feather",
+    iconName: "sun",
+  },
+  {
+    name: "cart",
+    iconType: "Feather",
+    iconName: "cart",
+  },
+];
---
+++ screens/Products.js
@@ -0,0 +1,22 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Products() {
+  return (
+    <View style={style.container}>
+      <Text style={style.title}>this is Home page </Text>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "700",
+  },
+});
